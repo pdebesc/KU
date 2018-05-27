e4d8387071495565a5a52c8e7d3a0bd2277c7552
Peter Debes Christensen <peter.christensen@edlund.dk>
2018-05-27 17:21:43 +0200

    initial commit

---
+++ AlgorithmsAndDatastructures/Opgave3/Program/Task1/Task1/BeerBarStuff/BeerBar.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace Task1.BeerBarStuff
+{
+  public class BeerBar
+  {
+    public BeerBar(int position, int beers, int demand)
+    {
+      Position = position;
+      Beers = beers;
+      Supply = beers - demand;
+    }
+
+    public int Position { get; }
+    public int Beers { get; private set; }
+
+    public int Supply { get; private set; }
+
+    //public void SetSupply(int demand)
+    //{
+    //  Supply = Beers - demand;
+    //}
+
+    public bool CanSupply(BeerBar otherBar, bool includeCost)
+    {
+      var cost  = includeCost ? transportationCost(otherBar) : 0;        
+      return Supply - cost > 0;
+    }
+
+    int transportationCost(BeerBar otherBar)
+    {
+      return 2 * Math.Abs(Position - otherBar.Position);
+    }
+
+    //private int canSend(BeerBar otherBar)
+    //{
+    //  var transportCost = transportationCost(otherBar);
+    //  var canSupply = Supply - transportCost;
+
+    //  var demand = Math.Abs(otherBar.Supply);
+    //  return otherBar.Supply < 0 && demand < canSupply ? demand : canSupply;
+    //}
+
+    public void SendSupply(BeerBar otherBar, bool includeCost)
+    {
+      var sendLeft = otherBar.Position < Position;
+      var transportCost = includeCost ? transportationCost(otherBar) : 0;
+      var inDemand = otherBar.Supply < 0;
+      var demand = inDemand ? -otherBar.Supply : 0;
+      var canSupply = Supply - transportCost;
+
+      var toSend = sendLeft ? Math.Min(demand, canSupply) : canSupply;
+
+      var actualSupplyAndTransportCost = toSend + transportCost;
+
+      otherBar.Supply += toSend;
+      otherBar.Beers += toSend;
+      Supply -= actualSupplyAndTransportCost;
+      Beers -= actualSupplyAndTransportCost;
+
+    }
+
+    //public void SendSupply(BeerBar otherBar)
+    //{
+    //  var supply = canSend(otherBar);
+
+    //  var transportCost = transportationCost(otherBar);
+
+    //  var actualSupplyAndTransportCost = supply + transportCost;
+
+    //  otherBar.Supply += supply;
+    //  otherBar.Beers += supply;
+    //  Supply -= actualSupplyAndTransportCost;
+    //  Beers -= actualSupplyAndTransportCost;
+    //}
+  }
+}
---
+++ AlgorithmsAndDatastructures/Opgave3/Program/Task1/Task1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Task1.BeerBarStuff;
+
+namespace Task1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      const int b = 22;
+
+      var positions = new[] { 0, 5, 13, 33, 36 };
+      var beers = new[] { 20, 40, 80, 10, 20 };
+      
+      var beerBars = new Queue<BeerBar>();
+
+      for (var i = 0; i < positions.Length; i++)
+      {
+        beerBars.Enqueue(new BeerBar(positions[i], beers[i], b));
+      }
+
+      var canSupplyB = CanSupplyB(b, beerBars);
+
+      Console.WriteLine("Can supply " + b + " :" + canSupplyB);
+      Console.ReadLine();
+    }
+
+    private static bool CanSupplyB(int b, Queue<BeerBar> beerBars)
+    {
+      var demands = new Queue<BeerBar>();
+      while (beerBars.Count > 0)
+      {
+        var beerBar = beerBars.Dequeue();
+
+        if (beerBar.Supply < 0)
+        {
+          demands.Enqueue(beerBar);
+          continue;
+        }
+
+        var includeCost = true;
+        while (demands.Count != 0 && beerBar.CanSupply(demands.Peek(), includeCost))
+        {
+          var previousBar = demands.Peek();
+          beerBar.SendSupply(previousBar, includeCost);
+          if (previousBar.Supply == 0)
+          {
+            demands.Dequeue();
+          }
+          includeCost = false;
+        }
+
+
+        if (beerBars.Count != 0 && beerBar.CanSupply(beerBars.Peek(), true))
+        {
+          beerBar.SendSupply(beerBars.Peek(), true);
+        }
+      }
+
+      return demands.Count == 0;
+    }
+  }
+}
